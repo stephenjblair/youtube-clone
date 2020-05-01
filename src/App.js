15168e718321474903a53dbca2579272307a4dd1
Stephen Blair <stephenjohnblair@outlook.com>
2020-05-01 22:48:11 +0100

    Set API base URL. Created SearchBar & VideoDetail components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import  { Grid } from '@material-ui/core';
+import { SearchBar, VideoDetail } from './components';
+import youtube from './api/youtube';
 
 class App extends React.Component {
     render() {
         return (
-        <h1>test</h1>
+            <Grid justify container spacing={16}>
+                <Grid xs={12}>
+                    <Grid container spacing={16}>
+                        <Grid item xs={12}>
+                            <SearchBar />
+                        </Grid>
+                        <Grid item xs={8}>
+                            <VideoDetail />
+                        </Grid>
+                        <Grid item xs={4}>
+                        </Grid>
+                    </Grid>
+
+                </Grid>
+
+            </Grid>
         )
     }
 }
